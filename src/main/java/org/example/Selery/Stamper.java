9fefb243b05b1fd7234f3dcf72f0859c285b7acb
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-01-07 22:16:39 +0200

    trying to make it work as intended

--- src/main/java/org/example/Selery/Stamper.java
+++ src/main/java/org/example/Selery/Stamper.java
@@ -10,12 +10,14 @@ public class Stamper<T> implements Visitor<T>{
     private static boolean inGroup = false;
     private static Integer ID = 1;
     private static List<String> stack = new ArrayList<String>();
+    private static int counter = 0;
     @Override
     public void onSignature(Task<T> sign) {
         sign.setHeader(ID.toString(), sign.getId());
         if(inGroup){
             stack.add(sign.getId());
             stack.add(ID.toString());
+            counter ++;
         }
         ID++;
     }
@@ -23,6 +25,9 @@ public class Stamper<T> implements Visitor<T>{
     @Override
     public void onGroupStart(Task<T> group) {
         onGroup = ID;
+        if(inGroup){
+
+        }
         inGroup = true;
         ID = 1;
     }
@@ -30,13 +35,14 @@ public class Stamper<T> implements Visitor<T>{
     @Override @SneakyThrows
     public void onGroupEnd(Task<T> group) {
         ID = onGroup;
-        while (!stack.isEmpty()){
+        while (counter!=0){
             String toSetID = stack.get(stack.size()-1);
             stack.remove(stack.size()-1);
             String toSetUUID = stack.get(stack.size()-1);
             stack.remove(stack.size()-1);
             group.setHeader(toSetID, toSetUUID);
+            counter--;
         }
-
+        inGroup = false;
     }
 }
