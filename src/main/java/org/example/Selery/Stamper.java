7b093c1127b95ea0d676ef4ac551fa2e1e78f005
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-01-08 14:30:22 +0200

    works?

--- src/main/java/org/example/Selery/Stamper.java
+++ src/main/java/org/example/Selery/Stamper.java
@@ -3,20 +3,25 @@ package org.example.Selery;
 import lombok.SneakyThrows;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Stamper<T> implements Visitor<T>{
     private static Integer onGroup;
-    private static boolean inGroup = false;
+    private static List<Boolean> inGroup = new ArrayList<>();
     private static Integer ID = 1;
+    private static List<HashMap<String, String>> nested = new ArrayList<>();
     private static List<String> stack = new ArrayList<String>();
     private static int counter = 0;
     @Override
     public void onSignature(Task<T> sign) {
-        sign.setHeader(ID.toString(), sign.getId());
-        if(inGroup){
-            stack.add(sign.getId());
-            stack.add(ID.toString());
+        sign.setHeader(sign.getId(), ID.toString());
+        if(!inGroup.isEmpty()){
+            HashMap<String, String> stack = nested.get(nested.size()-1);
+            stack.put(sign.getId(), ID.toString());
+//            stack.add(ID.toString());
+            nested.remove(nested.size()-1);
+            nested.add(stack);
             counter ++;
         }
         ID++;
@@ -25,24 +30,26 @@ public class Stamper<T> implements Visitor<T>{
     @Override
     public void onGroupStart(Task<T> group) {
         onGroup = ID;
-        if(inGroup){
-
-        }
-        inGroup = true;
+        nested.add(new HashMap<String, String>());
+        inGroup.add(true);
         ID = 1;
     }
 
     @Override @SneakyThrows
     public void onGroupEnd(Task<T> group) {
+
         ID = onGroup;
-        while (counter!=0){
-            String toSetID = stack.get(stack.size()-1);
-            stack.remove(stack.size()-1);
-            String toSetUUID = stack.get(stack.size()-1);
-            stack.remove(stack.size()-1);
-            group.setHeader(toSetID, toSetUUID);
-            counter--;
+        HashMap<String, String> toPut = nested.get(nested.size()-1);
+        nested.remove(nested.size()-1);
+        if (!nested.isEmpty()) {
+            HashMap<String, String> combines = nested.get(nested.size() - 1);
+            nested.remove(nested.size() - 1);
+            combines.putAll(toPut);
+            nested.add(combines);
         }
-        inGroup = false;
+        for(String key: toPut.keySet()){
+            group.setHeader(key, toPut.get(key));
+        }
+        inGroup.remove(inGroup.size()-1);
     }
 }
